4e361c0467d6b5f4440ff1277cf0e22e416311a0
timphisu <timothydelange3@gmail.com>
2026-02-16 19:09:44 -0800

    Quartz sync: Feb 16, 2026, 7:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -86,7 +86,7 @@ const config: QuartzConfig = {
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      // Plugin.CustomOgImages(),
     ],
   },
 }
